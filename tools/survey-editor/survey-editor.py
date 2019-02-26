7c2201c65ba3fcb561b62b2cf08a4c14e3edb42b
Philipp Feldner <feldnerphilipp@gmail.com>
2019-02-26 22:03:54 +0100

    base layout for mvc scheme in progress

--- tools/survey-editor/survey-editor.py
+++ tools/survey-editor/survey-editor.py
@@ -8,80 +8,21 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
+from model.survey import Survey
+from controllers.main_controller import MainController
+from views.main_view_ui import Ui_main_window
+from views.main_view import MainView
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(819, 439)
-        self.centralWidget = QtWidgets.QWidget(MainWindow)
-        self.centralWidget.setObjectName("centralWidget")
-        self.textEdit = QtWidgets.QTextEdit(self.centralWidget)
-        self.textEdit.setGeometry(QtCore.QRect(110, 60, 281, 81))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.textEdit.setFont(font)
-        self.textEdit.setObjectName("textEdit")
-        self.label = QtWidgets.QLabel(self.centralWidget)
-        self.label.setGeometry(QtCore.QRect(110, 40, 52, 14))
-        self.label.setObjectName("label")
-        self.textEdit_2 = QtWidgets.QTextEdit(self.centralWidget)
-        self.textEdit_2.setGeometry(QtCore.QRect(110, 170, 281, 81))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.textEdit_2.setFont(font)
-        self.textEdit_2.setObjectName("textEdit_2")
-        self.label_2 = QtWidgets.QLabel(self.centralWidget)
-        self.label_2.setGeometry(QtCore.QRect(110, 150, 52, 14))
-        self.label_2.setObjectName("label_2")
-        self.pushButton = QtWidgets.QPushButton(self.centralWidget)
-        self.pushButton.setGeometry(QtCore.QRect(110, 260, 80, 22))
-        self.pushButton.setObjectName("pushButton")
-        self.radioButton = QtWidgets.QRadioButton(self.centralWidget)
-        self.radioButton.setGeometry(QtCore.QRect(420, 60, 151, 20))
-        self.radioButton.setObjectName("radioButton")
-        MainWindow.setCentralWidget(self.centralWidget)
-        self.menuBar = QtWidgets.QMenuBar(MainWindow)
-        self.menuBar.setGeometry(QtCore.QRect(0, 0, 819, 19))
-        self.menuBar.setObjectName("menuBar")
-        self.menuQuestion_Editor = QtWidgets.QMenu(self.menuBar)
-        self.menuQuestion_Editor.setObjectName("menuQuestion_Editor")
-        MainWindow.setMenuBar(self.menuBar)
-        self.mainToolBar = QtWidgets.QToolBar(MainWindow)
-        self.mainToolBar.setObjectName("mainToolBar")
-        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
-        self.statusBar = QtWidgets.QStatusBar(MainWindow)
-        self.statusBar.setObjectName("statusBar")
-        MainWindow.setStatusBar(self.statusBar)
-        self.menuBar.addAction(self.menuQuestion_Editor.menuAction())
-        self.pushButton.clicked.connect(self.store)
-
-        self.textEdit.setPlainText("Enter text")
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Text"))
-        self.label_2.setText(_translate("MainWindow", "Choice"))
-        self.pushButton.setText(_translate("MainWindow", "Save"))
-        self.radioButton.setText(_translate("MainWindow", "Force Keyboard Reply"))
-        self.menuQuestion_Editor.setTitle(_translate("MainWindow", "File"))
-
-    def store(self):
-        print(self.textEdit.toPlainText())
-        print(self.textEdit_2.toPlainText())
-        print(self.radioButton.isChecked())
+class App(QApplication):
+    def __init__(self, sys_argv):
+        super(App, self).__init__(sys_argv)
+        self.model = Survey
+        self.main_controller = MainController(self.model)
+        self.main_view = MainView(self.model, self.main_controller)
+        self.main_view.show()
 
 
 if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-
-    app.exec_()
+    app = App(sys.argv)
+    sys.exit(app.exec_())
---
+++ tools/survey-editor/views/__init__.py
@@ -0,0 +1,2 @@
+from . import question_view_ui, main_view_ui
+__all__ = [question_view_ui, main_view_ui]
