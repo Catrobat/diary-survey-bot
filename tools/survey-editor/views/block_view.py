c82bc091a7b83a7a3975cf0542101ccbdcc58480
Philipp Feldner <feldnerphilipp@gmail.com>
2019-03-04 18:58:23 +0100

    main -> block -> question view transitions working

--- tools/survey-editor/views/block_view.py
+++ tools/survey-editor/views/block_view.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget
 
+from controllers.question_controller import QuestionController
 from views.block_view_ui import Ui_Block
 from views.question_view import QuestionView
 from model.survey import Question
@@ -16,3 +17,7 @@ class BlockView(QWidget):
         self._ui = Ui_Block()
         self._ui.setupUi(self)
         self._view = None
+
+        q_controller = QuestionController(self._model)
+        q = QuestionView(self._model, q_controller)
+        self._ui.tabWidget.addTab(q, self._model.default_language)
